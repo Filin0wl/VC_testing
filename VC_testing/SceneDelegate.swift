91bed126e20007ac92be8730f551b4f669470d93
Filin <nastia-dogf@mail.ru>
2022-07-08 12:52:06 +0300

    Added start page to navigation controller

--- VC_testing/SceneDelegate.swift
+++ VC_testing/SceneDelegate.swift
@@ -17,9 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+        let startViewController = StartViewController()
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController()
+        let navigationController = UINavigationController(rootViewController: startViewController)
         window.rootViewController = navigationController
         self.window = window
         window.makeKeyAndVisible()
