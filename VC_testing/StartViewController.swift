c1b8b769f263500942743d9e65820575586eda4b
Filin <nastia-dogf@mail.ru>
2022-07-08 12:05:55 +0300

    Added start controller view

---
+++ VC_testing/StartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartViewController.swift
+//  VC_testing
+//
+//  Created by Anastasia Perminova on 08.07.2022.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Start"
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
